7f8ae5aacd3c8e773eb15e29ce30e85dc10141ab
Ryan C. Gordon <icculus@icculus.org>
2021-03-01 15:25:02 +0000

    Fix external URLs.

--- pandoc-filter.lua
+++ pandoc-filter.lua
@@ -1,5 +1,8 @@
 function Link (link)
-  if link.target:sub(1,1) ~= '/' then
+  local absolute_path = link.target:sub(1,1) == '/'
+  local external_url = not absolute_path and link.target:find("%a+://") == 1
+  -- If it's not an absolute path and not an external URL, make it absolute.
+  if not absolute_path and not external_url then
     link.target = '/' .. link.target
   end
   return link
